7009b5dcd818a29fb8f076b1359f10b15cc8406f
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2016-10-05 11:29:09 +0200

    First commit

---
+++ src/ai2016/Group5.java
@@ -0,0 +1,83 @@
+package ai2016;
+
+import java.util.List;
+
+import negotiator.AgentID;
+import negotiator.Bid;
+import negotiator.Deadline;
+import negotiator.actions.Accept;
+import negotiator.actions.Action;
+import negotiator.actions.Offer;
+import negotiator.parties.AbstractNegotiationParty;
+import negotiator.session.TimeLineInfo;
+import negotiator.utility.AbstractUtilitySpace;
+
+/**
+ * This is your negotiation party.
+ */
+public class Group5 extends AbstractNegotiationParty {
+
+	private Bid lastReceivedBid = null;
+
+	@Override
+	public void init(AbstractUtilitySpace utilSpace, Deadline dl,
+			TimeLineInfo tl, long randomSeed, AgentID agentId) {
+
+		super.init(utilSpace, dl, tl, randomSeed, agentId);
+
+		System.out.println("Discount Factor is "
+				+ utilSpace.getDiscountFactor());
+		System.out.println("Reservation Value is "
+				+ utilSpace.getReservationValueUndiscounted());
+
+		// if you need to initialize some variables, please initialize them
+		// below
+
+	}
+
+	/**
+	 * Each round this method gets called and ask you to accept or offer. The
+	 * first party in the first round is a bit different, it can only propose an
+	 * offer.
+	 *
+	 * @param validActions
+	 *            Either a list containing both accept and offer or only offer.
+	 * @return The chosen action.
+	 */
+	@Override
+	public Action chooseAction(List<Class<? extends Action>> validActions) {
+
+		// with 50% chance, counter offer
+		// if we are the first party, also offer.
+		if (lastReceivedBid == null || !validActions.contains(Accept.class)
+				|| Math.random() > 0.5) {
+			return new Offer(getPartyId(), generateRandomBid());
+		} else {
+			return new Accept(getPartyId(), lastReceivedBid);
+		}
+	}
+
+	/**
+	 * All offers proposed by the other parties will be received as a message.
+	 * You can use this information to your advantage, for example to predict
+	 * their utility.
+	 *
+	 * @param sender
+	 *            The party that did the action. Can be null.
+	 * @param action
+	 *            The action that party did.
+	 */
+	@Override
+	public void receiveMessage(AgentID sender, Action action) {
+		super.receiveMessage(sender, action);
+		if (action instanceof Offer) {
+			lastReceivedBid = ((Offer) action).getBid();
+		}
+	}
+
+	@Override
+	public String getDescription() {
+		return "example party group N";
+	}
+
+}
